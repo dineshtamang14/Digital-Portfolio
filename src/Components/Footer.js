a48441a5d6f5da79390e893e67e31a0e80675d54
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-15 10:25:13 +0530

    added the footer section

---
+++ src/Components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made by{" "}
+              <a title="dinesh" href="https://github.com/dineshtamang14">
+                Dinesh
+              </a>
+            </li>
+            <li>&copy; since {currentYear}</li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
